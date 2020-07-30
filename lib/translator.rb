e74bd80deef799c6b2beac0bf552dbc912af3ad3
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-30 23:16:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,1 @@
 def load_library(file)
- emoticons = YAML.load_file(file)
-  emoticons.each do |english_word, emoticon_set|
-    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
-  end
-  emoticon_hash
-end
