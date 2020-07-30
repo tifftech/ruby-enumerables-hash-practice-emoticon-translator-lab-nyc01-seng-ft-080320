f58dea0cdebbda43c39490d7c65a0f740f1f8519
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-30 23:19:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,4 +2,6 @@ def load_library(path)
   emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
   YAML.load_file(path).each do |meaning, describe|
     eng, jan = describe
-    
+    emoticons["get_meaning"][jan] = meaning
+     emoticons["get_emoticon"][eng] = jan
+     
